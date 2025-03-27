2588abaf7e2bd642a4baeceec94c29361fdcfd7c
cristofer bolaños scalante <cristoferscalante@gmail.com>
2025-03-27 17:58:45 -0500

    27/03/2025

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { ProyectoModule } from './proyecto/proyecto.module';
+import { TareaModule } from './tarea/tarea.module';
+import { EventoModule } from './evento/evento.module';
+import { NotificacionModule } from './notificacion/notificacion.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // carga .env automáticamente
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/v1tro_db', {
+      dbName: 'v1tro_db',
+    }),
+    UsuarioModule,
+    ProyectoModule,
+    TareaModule,
+    EventoModule,
+    NotificacionModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
